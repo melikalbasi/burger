99ebcb88eaffa9f3d17db828be8fa706465963ad
Melika Kalbasi <melikalbasi@gmail.com>
2018-12-25 22:05:28 -0800

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double", false);
