99d62a903d2c0ac2f3c2c2235c04de4fb22d7085
Melika Kalbasi <melikalbasi@gmail.com>
2018-12-27 18:32:38 -0800

    finish server.js file

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 // Require npm packages
 var express = require("express");
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Serve static content for the app from public directory
+app.use(express.static("public"));
+
+// Parse application body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log server-side when our server has started
+  console.log("Server is listening on: http://localhost:" + PORT);
+});
